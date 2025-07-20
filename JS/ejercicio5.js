b330d79d4ac07d1a166332ec85baca1c99fa4b57
ignaciolp <ignaciolp@lepa.com.ar>
2025-07-20 11:52:01 -0300

    agrego los 3 botones

---
+++ JS/ejercicio5.js
@@ -0,0 +1,7 @@
+/* buscar los 3 botones */
+
+const btnIniciar = document.querySelector(`.btn-danger`);
+const btnPausar = document.querySelector(`.btn-primary`);
+const btnResetear = document.querySelector(`.btn-success`);
+
+let contador
