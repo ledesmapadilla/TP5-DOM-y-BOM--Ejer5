d2de8af94cea0ee8533cbd20560399c36fe23ced
ignaciolp <ignaciolp@lepa.com.ar>
2025-07-20 11:59:28 -0300

    empiezo con la funcionalidad de los botones

---
+++ JS/ejercicio5.js
@@ -0,0 +1,14 @@
+const btnIniciar = document.querySelector(`.btn-danger`);
+const btnPausar = document.querySelector(`.btn-primary`);
+const btnResetear = document.querySelector(`.btn-success`);
+
+let contador=0
+let idInterval;
+
+/* agregar funcionalidad - iniciar contador */
+
+btnIniciar.addEventListener(`click`, ()=> {
+  /* deshabilitar 2 botones */
+})
+
+
