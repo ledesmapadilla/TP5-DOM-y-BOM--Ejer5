0a574b661c0f545a143585995d690c21bc15d158
ignaciolp <ignaciolp@lepa.com.ar>
2025-07-22 12:48:43 -0300

    saco los botones pausar y resetear del de inicio

--- JS/ejercicio5.js
+++ JS/ejercicio5.js
@@ -3,7 +3,6 @@ const btnPausar = document.querySelector(`.btn-primary`);
 const btnResetear = document.querySelector(`.btn-success`);
 const textContador = document.querySelector(`.fw-semibold`);
 
-
 /* deshabilitar botones */
 
 const deshabilitarBotones = () => {
@@ -14,6 +13,7 @@ deshabilitarBotones();
 
 /* agregar funcionalidad - iniciar contador */
 let contador = 0;
+let idInterval;
 
 btnIniciar.addEventListener(`click`, () => {
   /* habilitar boton pausar */
@@ -23,39 +23,36 @@ btnIniciar.addEventListener(`click`, () => {
   btnResetear.disabled = true;
 
   /* arrancar contador */
-  
+
   console.log(textContador);
-  /* creo el contador */
-  /*  let contador = 0; */
-  let idInterval;
+  
+  
   /* inicio contador */
   idInterval = setInterval(() => {
     contador++;
     textContador.textContent = contador;
   }, 1000);
 
-  /* detengo contador */
-
-  btnPausar.addEventListener(`click`, () => {
-    /* habilito boton iniciar y resetear y deshabilito pausar*/
-    btnIniciar.disabled = false;
-    btnResetear.disabled = false;
-    btnPausar.disabled = true;
-    clearInterval(idInterval);
-  });
-
-  /* resetear cronometro */
-
-  btnResetear.addEventListener(`click`, () => {
-    /* habilito boton iniciar  y deshabilito resetear y pausar*/
-    btnIniciar.disabled = false;
-    btnResetear.disabled = true;
-    btnPausar.disabled = true;
-    console.log(`aqui resetear`);
-    console.log(contador);
-    /* pongo en cero contador */
-
-    textContador.textContent = `0`;
-  });
 });
- 
+
+
+btnPausar.addEventListener(`click`, () => {
+  /* habilito boton iniciar y resetear y deshabilito pausar*/
+  btnIniciar.disabled = false;
+  btnResetear.disabled = false;
+  btnPausar.disabled = true;
+  clearInterval(idInterval);
+});
+
+
+btnResetear.addEventListener(`click`, () => {
+  /* habilito boton iniciar  y deshabilito resetear y pausar*/
+  btnIniciar.disabled = false;
+  btnResetear.disabled = true;
+  btnPausar.disabled = true;
+  console.log(`aqui resetear`);
+  console.log(contador);
+  /* pongo en cero contador */
+
+  textContador.textContent = `0`;
+});
